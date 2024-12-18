6d9fdef64dc5ba2e247cd1104c26264f6a352e70
aryankumawat423 <129871919+aryankumawat423@users.noreply.github.com>
2024-12-19 02:32:24 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var tl = gsap.timeline()
+tl.from(".block",{
+    opacity:0,
+    y:20,
+    delay:2,
+    duration:3
+
+})
+tl.from(".block2",{
+    opacity:0,
+    y:20,
+    delay:2,
+    duration:3
+})
